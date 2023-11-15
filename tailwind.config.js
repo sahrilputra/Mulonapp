8328aec70e6dfa0a4842b197cde369f9482dce16
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-11-15 17:25:29 +0700

    checkpoint commit and building

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,6 @@ const config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   darkMode: "class",
