fe72bcb17d59fb50fee1e448d6a43ae694975eee
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2024-01-01 22:35:46 +0700

    removing testing

--- app/api/testing/route.js
+++ app/api/testing/route.js
@@ -1,12 +1,12 @@
 
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-import db from "../../../lib/db";
-export async function GET() {
-    db.connectDb();
-    db.disconnectDb();
-    return NextResponse.json({ name: 'john Doe' })
-}
+// import db from "../../../lib/db";
+// export async function GET() {
+//     db.connectDb();
+//     db.disconnectDb();
+//     return NextResponse.json({ name: 'john Doe' })
+// }
 
 // export async function GET() {
 //     return NextResponse.json({ name: 'Anuj Singh' });
